89ffd6016598fd4f36b1e6c8fb889281b3b0a9dd
Clement Delafargue <clement@delafargue.name>
2015-01-16 15:17:01 +0100

    log command
    `clever log <app-id>`. Displays the log stream of a given application.
    As of now it only displays new lines as they appear, not the one previously
    emitted.
    
    Ref #12

---
+++ src/log.js
@@ -0,0 +1,40 @@
+var path = require("path");
+
+var _ = require("lodash");
+var Bacon = require("baconjs");
+
+var Application = require("./models/application.js");
+var Log = require("./models/log.js");
+
+var Logger = require("./logger.js");
+
+var appLogs = module.exports = function(api) {
+  var yargs = appLogs.yargs();
+  var argv = yargs.argv;
+
+  if(argv.help) {
+    yargs.showHelp();
+    return;
+  }
+
+  var app_id = argv._[1];
+
+  var s_logs = Log.getAppLogs(app_id, api.session.getAuthorization());
+
+  s_logs.onValue(function(log) {
+    Logger.println(log._source["@timestamp"] + ": ", log._source["@message"]);
+  });
+  s_logs.onError(Logger.error);
+};
+
+appLogs.usage = "Usage: $0 logs <app-id>";
+appLogs.yargs = function() {
+  return require("yargs")
+    .usage(appLogs.usage)
+    .options("help", {
+      alias: "h",
+      boolean: true,
+      description: "Show an help message"
+    })
+    .demand(2);
+};
