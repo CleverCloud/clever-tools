7e620318e096ef3cf207898549461d84e35a5794
Clement Delafargue <clement@delafargue.name>
2015-08-19 12:24:11 +0200

    Use unified domain name for logs and events APIs

--- src/models/configuration.js
+++ src/models/configuration.js
@@ -18,8 +18,8 @@ var getConfigPath = function() {
 
 var conf = module.exports = env.getOrElseAll({
   API_HOST: "https://api.clever-cloud.com/v2",
-  LOG_URL: "wss://logs-api.clever-cloud.com/logs-socket/<%- appId %>?since=<%- timestamp %>",
-  EVENT_URL: "wss://event-api-clever-cloud.cleverapps.io/event-socket/",
+  LOG_URL: "wss://api.clever-cloud.com/v2/logs/logs-socket/<%- appId %>?since=<%- timestamp %>",
+  EVENT_URL: "wss://api.clever-cloud.com/v2/events/event-socket",
   OAUTH_CONSUMER_KEY: "DVXgEDKLATkZkSRqN7iQ0KwWSvtNaD",
   OAUTH_CONSUMER_SECRET: "GPKbDuphYWFr3faS5dg64eCjsrpxGY",
 
