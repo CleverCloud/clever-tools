6ce369ff946be669abb3eabf940e036d3a2a8a56
BlackYoup <arnaud.lefebvre@clever-cloud.com>
2015-09-03 11:10:40 +0200

    Fix getAuthorization tokens keys names

--- src/models/api.js
+++ src/models/api.js
@@ -6,21 +6,21 @@ module.exports = function() {
   var s_oauthData = conf.loadOAuthConf();
 
   var s_api = s_oauthData.map(function(tokens) {
-    var userTokens = {
-      API_OAUTH_TOKEN: tokens.token,
-      API_OAUTH_TOKEN_SECRET: tokens.secret
-    };
-
     var api = require("clever-client")(_.defaults(conf, {
       API_HOST: conf.API_HOST,
       API_CONSUMER_KEY: conf.OAUTH_CONSUMER_KEY,
       API_CONSUMER_SECRET: conf.OAUTH_CONSUMER_SECRET,
+      API_OAUTH_TOKEN: tokens.token,
+      API_OAUTH_TOKEN_SECRET: tokens.secret,
       logger: Logger
-    }, userTokens));
+    }));
 
     // Waiting for clever-client to be fully node compliant
     api.session.getAuthorization = function(httpMethod, url, params) {
-      return api.session.getHMACAuthorization(httpMethod, url, params, userTokens);
+      return api.session.getHMACAuthorization(httpMethod, url, params, {
+        user_oauth_token: tokens.token,
+        user_oauth_token_secret: tokens.secret
+      });
     };
 
     return api;
