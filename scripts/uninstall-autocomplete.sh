c356ecd4d3e95bc16458fda403e3214f4ca0ea0f
Clement Delafargue <clement@delafargue.name>
2015-09-09 16:28:24 +0200

    Uninstall bash & zsh completion scripts when installed as root

--- scripts/uninstall-autocomplete.sh
+++ scripts/uninstall-autocomplete.sh
@@ -1,13 +1,30 @@
 #!/usr/bin/env sh
 
-uninstall_zsh() {
+uninstall_zsh_user() {
  local zsh_comp_dir="${HOME}"/.local/share/zsh/site-functions
  rm "${zsh_comp_dir}"/_clever || true
 }
 
+uninstall_zsh_root() {
+ rm /usr/share/zsh/site-functions/_clever || true
+}
+
+uninstall_bash_root() {
+ rm /usr/share/bash-completion/completions/clever || true
+}
+
 uninstall() {
- if which zsh &>/dev/null; then
-   uninstall_zsh
+ if [[ "$USER" = "root" ]]; then
+  if which zsh &>/dev/null; then
+    uninstall_zsh_root
+  fi
+  if which bash &>/dev/null; then
+    uninstall_bash_root
+  fi
+ else
+  if which zsh &>/dev/null; then
+    uninstall_zsh_user
+  fi
  fi
 }
 
