197517c9fecc14fcd997487371b54c1895055012
Clement Delafargue <clement@delafargue.name>
2015-09-09 16:20:20 +0200

    Autocomplete support for users installing clever-tools as root

--- scripts/install-autocomplete.sh
+++ scripts/install-autocomplete.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-install_bash() {
+install_bash_user() {
  local us="${1}"
 
  clever --bash-autocomplete-script "${us}" >> "${HOME}"/.bash_completion
 }
 
-install_zsh() {
+install_zsh_user() {
  local us="${1}"
  local zsh_comp_dir="${HOME}"/.local/share/zsh/site-functions
 
@@ -18,14 +18,35 @@ install_zsh() {
  fi
 }
 
+install_bash_root() {
+ local us="${1}"
+
+ clever --bash-autocomplete-script "${us}" >> /usr/share/bash-completion/completions/clever
+}
+
+install_zsh_root() {
+ local us="${1}"
+
+ clever --zsh-autocomplete-script "${us}" >> /usr/share/zsh/site-functions/_clever
+}
+
 install() {
  local us="$(which clever)"
 
- if which bash &>/dev/null; then
-   install_bash "${us}"
- fi
- if which zsh &>/dev/null; then
-   install_zsh "${us}"
+ if [[ "$USER" = "root" ]]; then
+   if which bash &>/dev/null; then
+     install_bash_root "${us}"
+   fi
+   if which zsh &>/dev/null; then
+     install_zsh_root "${us}"
+   fi
+ else
+   if which bash &>/dev/null; then
+     install_bash_user "${us}"
+   fi
+   if which zsh &>/dev/null; then
+     install_zsh_user "${us}"
+   fi
  fi
 }
 
