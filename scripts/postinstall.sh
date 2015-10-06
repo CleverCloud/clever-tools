e85bc2632d02dc30ac4e74bc07789b007f384918
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2015-10-06 12:35:49 +0200

    postinstall: fix quoting
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-cat <<"EOF"
+cat << EOF
 To enable autocompletion, run 'install-clever-completion' (needs sudo)
 If you don't have sudo or want to install completion manually,
 
-    clever --bash-autocomplete-script '$(which clever)' > /usr/share/bash-completion/completions/clever
-    clever --zsh-autocomplete-script '$(which clever)' > /usr/share/zsh/site-functions/_clever
+    clever --bash-autocomplete-script "\$(which clever)" > /usr/share/bash-completion/completions/clever
+    clever --zsh-autocomplete-script "\$(which clever)" > /usr/share/zsh/site-functions/_clever
 
 You can uninstall autocompletion at any moment by running 'uninstall-clever-completion'
 EOF
