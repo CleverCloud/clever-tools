636dfab7e55f24ac0e38077cacdbe97404363fdc
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2015-10-07 07:58:19 +0200

    install-autocomplete: use a wrapper not to run clever as root
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- scripts/install-autocomplete.sh
+++ scripts/install-autocomplete.sh
@@ -3,33 +3,36 @@
 set -euo pipefail
 
 install_bash_root() {
-  local us="${1}"
+  local wrapper="${1}"
+  local us="${2}"
   local compdir="/usr/share/bash-completion/completions"
 
   echo "Installing bash completion script"
   mkdir -p "${compdir}"
-  clever --bash-autocomplete-script "${us}" > "${compdir}"/clever
+  clever --bash-autocomplete-script "${us}" | "${wrapper}" tee "${compdir}"/clever >/dev/null
 }
 
 install_zsh_root() {
-  local us="${1}"
+  local wrapper="${1}"
+  local us="${2}"
   local compdir="/usr/share/zsh/site-functions"
 
   echo "Installing zsh completion script"
   mkdir -p "${compdir}"
-  clever --zsh-autocomplete-script "${us}" > "${compdir}"/_clever
+  clever --zsh-autocomplete-script "${us}" | "${wrapper}" tee "${compdir}"/_clever >/dev/null
 }
 
 install() {
+  local wrapper="${1}"
   local us
 
   us="$(which clever)"
 
   if which bash >/dev/null 2>&1; then
-    install_bash_root "${us}"
+    install_bash_root "${wrapper}" "${us}"
   fi
   if which zsh >/dev/null 2>&1; then
-    install_zsh_root "${us}"
+    install_zsh_root "${wrapper}" "${us}"
   fi
 
   cat <<"EOF"
@@ -38,12 +41,21 @@ EOF
 }
 
 main() {
-    if [ "$(id -u)" -ne 0 ]; then
-        echo "This program requires root privileges" >&2
-        echo "Please run it as root or try either 'pkexec ${0}' or 'sudo ${0}'" >&2
-        exit 1
-    fi
-    install
+  local wrapper
+  local wrappers=( pkexec sudo )
+
+  for w in "${wrappers[@]}"; do
+    wrapper="$(which "${w}" 2>/dev/null || true)"
+    [ "x${wrapper}" != "x" ] && break
+  done
+
+  if [ "x${wrapper}" == "x" ]; then
+    echo "This program requires root privileges and neither of \"${wrappers[@]}\" is installed" >&2
+    echo "Please run it as root" >&2
+    exit 1
+  fi
+
+  install "${wrapper}"
 }
 
 main
