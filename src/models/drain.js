b5296d52201b91227b43d9745224bc2e4b360052
Alexandre DUVAL <alexandre.duval@clever-cloud.com>
2017-06-22 13:32:39 +0200

    refactor routes to match with logsapi

--- src/models/drain.js
+++ src/models/drain.js
@@ -9,82 +9,74 @@ var conf = require("./configuration.js");
 var Drain = module.exports;
 
 var makeJsonRequest = function(api, verb, url, queryParams, body) {
-  var completeUrl = conf.API_HOST + url
-  Logger.debug(verb + ' ' + completeUrl);
-  var options = {
-    method: verb,
-    url: completeUrl,
-    headers: {
-      "Authorization": api.session.getAuthorization(verb, completeUrl, queryParams),
-      "Accept": "application/json"
-    }
-  };
-  if(completeUrl.substring(0,8) === 'https://')
-    options.agent = new (require("https").Agent)({ keepAlive: true })
+    var completeUrl = conf.API_HOST + url;
+    Logger.debug(verb + ' ' + completeUrl);
+    var options = {
+        method: verb,
+        url: completeUrl,
+        headers: {
+            "Authorization": api.session.getAuthorization(verb, completeUrl, queryParams),
+            "Accept": "application/json"
+        }
+    };
+    if (completeUrl.substring(0, 8) === 'https://')
+        options.agent = new(require("https").Agent)({ keepAlive: true })
 
-  if(body) options.json = body;
+    if (body) options.json = body;
 
-  var s_res = Bacon.fromNodeCallback(request, options);
+    var s_res = Bacon.fromNodeCallback(request, options);
 
-  return s_res.flatMapLatest(function(res) {
-    if(res.statusCode >= 400) {
-      return new Bacon.Error(res.body);
-    }
-    if(typeof res.body === "object") return res.body;
+    return s_res.flatMapLatest(function(res) {
+        if (res.statusCode >= 400) {
+            return new Bacon.Error(res.body);
+        }
+        if (typeof res.body === "object") return res.body;
 
-    var jsonBody = _.attempt(JSON.parse, res.body);
-    if(!_.isError(jsonBody) && _.isArray(jsonBody)) {
-      return jsonBody;
-    } else {
-      if(!_.isError(jsonBody) && jsonBody["type"] === "error") {
-        return new Bacon.Error(jsonBody);
-      }
-    }
-  });
+        var jsonBody = _.attempt(JSON.parse, res.body);
+        if (!_.isError(jsonBody) && _.isArray(jsonBody)) {
+            return jsonBody;
+        } else {
+            if (!_.isError(jsonBody) && jsonBody.type === "error") {
+                return new Bacon.Error(jsonBody);
+            }
+        }
+    });
 }
 
 Drain.list = function(api, appId) {
-  Logger.debug("Fetching drains for " + appId);
-  var s_res = makeJsonRequest(api, 'GET', '/logs/drains/' + appId, {});
-  return s_res.map(function(drains) {
-    return drains
-  });
+    Logger.debug("Fetching drains for " + appId);
+    var s_res = makeJsonRequest(api, 'GET', '/logs/' + appId + '/drains', {});
+    return s_res.map(function(drains) {
+        return drains;
+    });
 };
 
 Drain.create = function(api, appId, drainTargetURL, drainTargetType, drainTargetCredentials) {
-  Logger.debug("Registering drain for " + appId);
-  var body = {}
-  if (drainTargetCredentials) {
-    body = {
-      "url": drainTargetURL,
-      "drainType": drainTargetType,
-      "credentials": {
-        "username": drainTargetCredentials.username,
-        "password": drainTargetCredentials.password
-      }
-    }    
-  } else {
-    body = {
-      "url": drainTargetURL,
-      "drainType": drainTargetType
-    }
-  }
+    Logger.debug("Registering drain for " + appId);
+    var body = {
+        "url": drainTargetURL,
+        "drainType": drainTargetType,
+        "credentials": {
+            "username": drainTargetCredentials.username || "",
+            "password": drainTargetCredentials.password || ""
+        }
+    };
 
-  var s_res = makeJsonRequest(api, 'POST', '/logs/drains/' + appId, {}, body);
-  return s_res;
+    var s_res = makeJsonRequest(api, 'POST', '/logs/' + appId + '/drains', {}, body);
+    return s_res;
 };
 
 Drain.remove = function(api, appId, drainId) {
-  Logger.debug("Removing drain " + drainId + " for " + appId);
-  var s_res = makeJsonRequest(api, 'DELETE', '/logs/drains/' + appId + '/' + drainId, {});
-  return s_res;
+    Logger.debug("Removing drain " + drainId + " for " + appId);
+    var s_res = makeJsonRequest(api, 'DELETE', '/logs/' + appId + '/drains/' + drainId, {});
+    return s_res;
 };
 
 Drain.listMetaEvents = function() {
-  return autocomplete.words([
-   "META_SERVICE_LIFECYCLE",
-   "META_DEPLOYMENT_RESULT",
-   "META_SERVICE_MANAGEMENT",
-   "META_CREDITS"
-  ]);
+    return autocomplete.words([
+        "META_SERVICE_LIFECYCLE",
+        "META_DEPLOYMENT_RESULT",
+        "META_SERVICE_MANAGEMENT",
+        "META_CREDITS"
+    ]);
 };
