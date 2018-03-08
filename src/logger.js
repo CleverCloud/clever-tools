33bcac474ae577afc8b6ec2f81a7a8ea503b8692
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2018-03-08 12:00:40 +0100

    Refactor logger.js

--- src/logger.js
+++ src/logger.js
@@ -1,33 +1,41 @@
-'use strict'
+'use strict';
 
-const _ = require('lodash')
+const _ = require('lodash');
 
-const processApiError = function (error) {
-  if (error.id == null || error.message == null) {
-    return error
-  }
-  const fields = _.map(error.fields, (msg, field) => `${field}: ${msg}`)
-  return [`${error.message} [${error.id}]`, ...fields].join('\n')
+function getPrefix (severity) {
+  return `[${severity.toUpperCase()}] `;
 }
+
+function processApiError (error) {
+  if (error.id == null || error.message == null) {
+    return error;
+  }
+  const fields = _.map(error.fields, (msg, field) => `${field}: ${msg}`);
+  return [`${error.message} [${error.id}]`, ...fields].join('\n');
+};
 
 const Logger = _(['debug', 'info', 'warn', 'error'])
   .map((severity) => {
     if (process.env['CLEVER_QUIET'] || !process.env['CLEVER_VERBOSE'] && (severity === 'debug' || severity === 'info')) {
-      return [severity, _.noop]
+      return [severity, _.noop];
     }
-    const consoleFn = (severity === 'error') ? console.error : console.log
-    return [severity, (error) => consoleFn(`[${severity.toUpperCase()}]`, processApiError(error))]
+    const consoleFn = (severity === 'error') ? console.error : console.log;
+    const prefix = getPrefix(severity);
+    return [severity, (message) => {
+      const formattedMsg = processApiError(message);
+      return consoleFn(`${prefix}${formattedMsg}`);
+    }];
   })
   .fromPairs()
-  .value()
+  .value();
 
 // No decoration for Logger.println
-Logger.println = console.log
+Logger.println = console.log;
 
 // No decoration for Logger.printErrorLine
-Logger.printErrorLine = console.error
+Logger.printErrorLine = console.error;
 
 // Only exported for testing, shouldn't be used directly
-Logger.processApiError = processApiError
+Logger.processApiError = processApiError;
 
-module.exports = Logger
+module.exports = Logger;
