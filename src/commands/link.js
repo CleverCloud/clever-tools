1cb896eb6f32877702481da004d33b0656584966
kriyss <kriyssou@gmail.com>
2018-06-28 12:04:46 +0200

    fix: return error code on link error

--- src/commands/link.js
+++ src/commands/link.js
@@ -5,23 +5,21 @@ var nodegit = require("nodegit");
 
 var Logger = require("../logger.js");
 
+var handleCommandStream = require('../command-stream-handler');
 var Application = require("../models/application.js");
 var Git = require("../models/git.js")(path.resolve("."));
 
-var link = module.exports = function(api, params) {
+var link = module.exports = function (api, params) {
   var appIdOrName = params.args[0];
   var orgaIdOrName = params.options.org;
   var alias = params.options.alias;
 
-  if(appIdOrName.app_id && orgaIdOrName) {
+  if (appIdOrName.app_id && orgaIdOrName) {
     Logger.warn("You've specified a unique application ID, organisation option will be ignored");
   }
 
   var s_app = Application.linkRepo(api, appIdOrName, orgaIdOrName, alias);
 
-  s_app.onValue(function(app) {
-    Logger.println("Your application has been successfully linked!");
-  });
 
-  s_app.onError(Logger.error);
+  handleCommandStream(s_app, app => Logger.println("Your application has been successfully linked!"));
 };
