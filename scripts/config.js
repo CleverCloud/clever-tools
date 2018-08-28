2bd1a16253057a52bde93ed5c4f276349d3f5ec2
muhqpfrizf <muhqpfrizf@email-address-not-set>
2018-08-28 13:56:27 +0000

    Added Jenkinsfile

---
+++ scripts/config.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const semver = require('semver');
+
+const archList = ['linux', 'macos', 'win'];
+const nodeVersion = process.versions.node;
+const releasesDir = 'releases';
+const cellar = {
+  host: 'cellar.services.clever-cloud.com',
+  bucket: 'clever-tools',
+};
+const bintray = {
+  subject: 'clevercloud',
+  user: 'ci-clevercloud',
+};
+const git = {
+  email: 'ci@clever-tools.com',
+  name: 'Clever Cloud CI',
+};
+const appInfos = {
+  name: 'clever-tools',
+  vendor: 'Clever Cloud',
+  url: 'https://github.com/CleverCloud/clever-tools',
+  description: 'Command Line Interface for Clever Cloud.',
+  license: 'MIT',
+  maintainer: `${git.name} <${git.email}>`,
+};
+
+function getVersion (striclyFromTag = false) {
+  const version = process.env.GIT_TAG_NAME;
+  if (!version) {
+    if (striclyFromTag) {
+      throw new Error('Could not read version from git tag!');
+    }
+    return process.env.GIT_BRANCH;
+  }
+  return version.trim();
+}
+
+function isStableVersion () {
+  const version = getVersion(true);
+  return semver.prerelease(version).length === 0;
+}
+
+function getBinaryFilename (arch) {
+  return (arch === 'win') ? `clever.exe` : 'clever';
+}
+
+function getBinaryFilepath (arch) {
+  const version = getVersion();
+  const filename = getBinaryFilename(arch);
+  return `${releasesDir}/${version}/${arch}/${filename}`;
+}
+
+function getArchiveFilepath (arch) {
+  const version = getVersion();
+  const archiveExt = (arch === 'win') ? '.zip' : '.tar.gz';
+  return `${releasesDir}/${version}/${appInfos.name}-${version}_${arch}${archiveExt}`;
+}
+
+function getBundleFilepath (type) {
+  const version = getVersion();
+  return `${releasesDir}/${version}/${appInfos.name}-${version}.${type}`;
+}
+
+function getBintrayApiKey () {
+  const apiKey = process.env.BINTRAY_API_KEY;
+  if (!apiKey) {
+    throw new Error('Could not read bintray API key!');
+  }
+  return apiKey;
+}
+
+module.exports = {
+  archList,
+  nodeVersion,
+  releasesDir,
+  cellar,
+  bintray,
+  git,
+  appInfos,
+  getVersion,
+  isStableVersion,
+  getBinaryFilepath,
+  getArchiveFilepath,
+  getBundleFilepath,
+  getBintrayApiKey,
+};
---
+++ scripts/job-build.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const cfg = require('./config');
+const del = require('del');
+const pkg = require('pkg').exec;
+
+async function run () {
+
+  const { archList, nodeVersion, releasesDir } = cfg;
+
+  del.sync(releasesDir);
+
+  for (let arch of archList) {
+    console.log(`Building pkg for ${arch} ...\n`);
+    const filepath = cfg.getBinaryFilepath(arch);
+    await pkg([`.`, `-t`, `node${nodeVersion}-${arch}`, `-o`, filepath]);
+    console.log(`\nBuilding pkg for ${arch} DONE!\n`);
+  }
+}
+
+run().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
