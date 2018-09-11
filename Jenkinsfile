28ef0c3fcf69a25f62bef6d8747d0a2ec31470c7
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2018-09-11 11:21:47 +0200

    Enable new multibranch pipeline with a Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,76 @@
+pipeline {
+  agent { label 'cc-ci-agent' }
+  environment {
+    GIT_TAG_NAME = gitTagName()
+    S3_KEY_ID = credentials('CELLAR_CC_TOOLS_ACCESS_KEY_ID')
+    S3_SECRET_KEY = credentials('CELLAR_CC_TOOLS_SECRET_ACCESS_KEY')
+    BINTRAY_API_KEY = credentials('BINTRAY_CC_TOOLS_API_KEY')
+    NPM_TOKEN = credentials('NPM_TOKEN')
+  }
+  stages {
+    stage('build') {
+      steps {
+        sh 'npm install'
+        sh 'node scripts/job-build.js'
+      }
+    }
+    stage('package') {
+      steps {
+        sh 'node scripts/job-package.js'
+      }
+    }
+    stage('publish') {
+      when {
+        not {
+          environment name: 'GIT_TAG_NAME', value: ''
+        }
+        beforeAgent true
+      }
+      parallel {
+        stage('cellar') {
+          steps {
+            sh 'node scripts/job-publish-cellar.js'
+          }
+        }
+        stage('bintray') {
+          steps {
+            sh 'node scripts/job-publish-bintray.js'
+          }
+        }
+        stage('arch') {
+          steps {
+            script {
+              sshagent (credentials: ['CI_CLEVER_CLOUD_SSH_KEY']) {
+                sh 'node ./scripts/job-publish-arch.js'
+              }
+            }
+          }
+        }
+        stage('brew') {
+          steps {
+            script {
+              sshagent (credentials: ['CI_CLEVER_CLOUD_SSH_KEY']) {
+                sh 'node ./scripts/job-publish-brew.js'
+              }
+            }
+          }
+        }
+        stage('npm') {
+          steps {
+            sh 'node ./scripts/job-publish-npm.js'
+          }
+        }
+      }
+    }
+  }
+  post {
+    always {
+      archiveArtifacts artifacts: 'releases/**/*', fingerprint: true
+    }
+  }
+}
+
+@NonCPS
+String gitTagName() {
+    return sh(script: 'git describe --tags --exact-match $(git rev-parse HEAD) || true', returnStdout: true)?.trim()
+}
---
+++ scripts/config.js
@@ -0,0 +1,104 @@
+'use strict';
+
+const pkgJson = require('../package.json');
+const semver = require('semver');
+
+const archList = ['linux', 'macos', 'win'];
+const nodeVersion = process.versions.node;
+const releasesDir = 'releases';
+const cellar = {
+  host: 'cellar.services.clever-cloud.com',
+  bucket: 'clever-tools',
+};
+const bintray = {
+  subject: 'clevercloud',
+  user: 'ci-clevercloud',
+};
+const git = {
+  email: 'ci@clever-cloud.com',
+  name: 'Clever Cloud CI',
+};
+const appInfos = {
+  name: pkgJson.name,
+  vendor: 'Clever Cloud',
+  url: pkgJson.homepage,
+  description: pkgJson.description,
+  license: pkgJson.license,
+  maintainer: `${git.name} <${git.email}>`,
+  keywords: pkgJson.keywords.join(' '),
+};
+
+function getVersion (striclyFromTag = false) {
+  const gitTag = process.env.GIT_TAG_NAME;
+  if (!gitTag) {
+    if (striclyFromTag) {
+      throw new Error('Could not read version from git tag!');
+    }
+    return process.env.GIT_BRANCH;
+  }
+  const gitTagVersion = gitTag.trim();
+  if (gitTagVersion !== pkgJson.version) {
+    throw new Error(`Mismatch between git tag ${gitTagVersion} and package.json version ${pkgJson.version}`);
+  }
+  return gitTagVersion;
+}
+
+function isStableVersion () {
+  const version = getVersion(true);
+  return semver.prerelease(version).length === 0;
+}
+
+function getBinaryFilename (arch) {
+  return (arch === 'win') ? `clever.exe` : 'clever';
+}
+
+function getBinaryFilepath (arch) {
+  const version = getVersion();
+  const filename = getBinaryFilename(arch);
+  return `${releasesDir}/${version}/${arch}/${filename}`;
+}
+
+function getArchiveFilepath (arch) {
+  const version = getVersion();
+  const archiveExt = (arch === 'win') ? '.zip' : '.tar.gz';
+  return `${releasesDir}/${version}/${appInfos.name}-${version}_${arch}${archiveExt}`;
+}
+
+function getBundleFilepath (type) {
+  const version = getVersion();
+  const nameVersionDelimiter = (type === 'nupkg') ? '.' : '-';
+  return `${releasesDir}/${version}/${appInfos.name}${nameVersionDelimiter}${version}.${type}`;
+}
+
+function getBintrayApiKey () {
+  const apiKey = process.env.BINTRAY_API_KEY;
+  if (!apiKey) {
+    throw new Error('Could not read bintray API key!');
+  }
+  return apiKey;
+}
+
+function getNpmToken () {
+  const token = process.env.NPM_TOKEN;
+  if (!token) {
+    throw new Error('Could not read NPM token!');
+  }
+  return token;
+}
+
+module.exports = {
+  archList,
+  nodeVersion,
+  releasesDir,
+  cellar,
+  bintray,
+  git,
+  appInfos,
+  getVersion,
+  isStableVersion,
+  getBinaryFilepath,
+  getArchiveFilepath,
+  getBundleFilepath,
+  getBintrayApiKey,
+  getNpmToken,
+};
