05db818bf947c4c339bd108e6a724ec86b3c1452
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2019-08-01 13:31:58 +0200

    Refactor send-to-api token loading

--- src/models/send-to-api.js
+++ src/models/send-to-api.js
@@ -5,29 +5,29 @@ const { prefixUrl } = require('@clevercloud/client/cjs/prefix-url.js');
 const { request } = require('@clevercloud/client/cjs/request.superagent.js');
 const { conf, loadOAuthConf } = require('../models/configuration.js');
 
-async function sendToApi (requestParams) {
+async function loadTokens () {
   const tokens = await loadOAuthConf().toPromise();
+  return {
+    OAUTH_CONSUMER_KEY: conf.OAUTH_CONSUMER_KEY,
+    OAUTH_CONSUMER_SECRET: conf.OAUTH_CONSUMER_SECRET,
+    API_OAUTH_TOKEN: tokens.token,
+    API_OAUTH_TOKEN_SECRET: tokens.secret,
+  };
+}
+
+async function sendToApi (requestParams) {
+  const tokens = await loadTokens();
   return Promise.resolve(requestParams)
     .then(prefixUrl(conf.API_HOST))
-    .then(addOauthHeader({
-      OAUTH_CONSUMER_KEY: conf.OAUTH_CONSUMER_KEY,
-      OAUTH_CONSUMER_SECRET: conf.OAUTH_CONSUMER_SECRET,
-      API_OAUTH_TOKEN: tokens.token,
-      API_OAUTH_TOKEN_SECRET: tokens.secret,
-    }))
+    .then(addOauthHeader(tokens))
     .then(request);
 }
 
 async function getHostAndTokens () {
-  const userTokens = await loadOAuthConf().toPromise();
+  const tokens = await loadTokens();
   return {
     apiHost: conf.API_HOST,
-    tokens: {
-      OAUTH_CONSUMER_KEY: conf.OAUTH_CONSUMER_KEY,
-      OAUTH_CONSUMER_SECRET: conf.OAUTH_CONSUMER_SECRET,
-      API_OAUTH_TOKEN: userTokens.token,
-      API_OAUTH_TOKEN_SECRET: userTokens.secret,
-    },
+    tokens,
   };
 }
 
