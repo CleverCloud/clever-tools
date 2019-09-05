751e1a1af72090ad9136b27fa3290508f8994b5a
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2019-09-05 19:00:25 +0200

    make `clever open` default to https://fqdn

--- src/commands/open.js
+++ src/commands/open.js
@@ -11,7 +11,7 @@ async function open (params) {
   const { org_id, app_id: appId } = await AppConfig.getAppData(alias).toPromise();
 
   const vhost = await Domain.getBest(appId, org_id);
-  const url = 'http://' + vhost.fqdn;
+  const url = 'https://' + vhost.fqdn;
 
   Logger.println('Opening the application in your browser');
   await openPage(url, { wait: false });
