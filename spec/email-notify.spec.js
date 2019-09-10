f684c4ca4b1492c6f609229b4ac5074003339b4e
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2019-09-10 16:34:19 +0200

    Fix notify-email tests

--- spec/email-notify.spec.js
+++ spec/email-notify.spec.js
@@ -1,48 +1,46 @@
-var path = require("path");
+'use strict';
 
-var _ = require("lodash");
-var Bacon = require("baconjs");
-var expect = require('chai').expect;
+const expect = require('chai').expect;
 
-var notifications = require("../src/commands/notifications.js");
+const notifications = require('../src/commands/notify-email.js');
 
-describe("notifications.getEmailNotificationTargets", function() {
+describe('notifications.getEmailNotificationTargets', function () {
 
-  it("handle default case", function() {
-    var input = {options: {}};
-    var output = notifications.getEmailNotificationTargets(input);
-    var expected = [];
+  it('handle default case', function () {
+    const input = null;
+    const output = notifications.getEmailNotificationTargets(input);
+    const expected = [];
     expect(output).to.deep.equal(expected);
   });
 
-  it("handle single user id", function() {
-    var input = {options: { "notify": "user_20ebac08-8531-4296-98c9-8dbc7abcd3f4"}};
-    var output = notifications.getEmailNotificationTargets(input);
-    var expected = [ { "type": "userid", "target": "user_20ebac08-8531-4296-98c9-8dbc7abcd3f4" } ];
+  it('handle single user id', function () {
+    const input = ['user_20ebac08-8531-4296-98c9-8dbc7abcd3f4'];
+    const output = notifications.getEmailNotificationTargets(input);
+    const expected = [{ type: 'userid', target: 'user_20ebac08-8531-4296-98c9-8dbc7abcd3f4' }];
     expect(output).to.deep.equal(expected);
   });
 
-  it("handle single email address", function() {
-    var input = {options: { "notify": "test@example.org" }};
-    var output = notifications.getEmailNotificationTargets(input);
-    var expected = [ { "type": "email", "target": "test@example.org" } ];
+  it('handle single email address', function () {
+    const input = ['test@example.org'];
+    const output = notifications.getEmailNotificationTargets(input);
+    const expected = [{ type: 'email', target: 'test@example.org' }];
     expect(output).to.deep.equal(expected);
   });
 
-  it("handle single organisation token", function() {
-    var input = {options: { "notify": "organisation" }};
-    var output = notifications.getEmailNotificationTargets(input);
-    var expected = [ { "type": "organisation" } ];
+  it('handle single organisation token', function () {
+    const input = ['organisation'];
+    const output = notifications.getEmailNotificationTargets(input);
+    const expected = [{ type: 'organisation' }];
     expect(output).to.deep.equal(expected);
   });
 
-  it("handle mixed targets", function() {
-    var input = {options: { "notify": "organisation,test@example.org,user_20ebac08-8531-4296-98c9-8dbc7abcd3f4" }};
-    var output = notifications.getEmailNotificationTargets(input);
-    var expected = [
-      { "type": "organisation" },
-      { "type": "email", "target": "test@example.org" },
-      { "type": "userid", "target": "user_20ebac08-8531-4296-98c9-8dbc7abcd3f4" }
+  it('handle mixed targets', function () {
+    const input = ['organisation', 'test@example.org', 'user_20ebac08-8531-4296-98c9-8dbc7abcd3f4'];
+    const output = notifications.getEmailNotificationTargets(input);
+    const expected = [
+      { type: 'organisation' },
+      { type: 'email', target: 'test@example.org' },
+      { type: 'userid', target: 'user_20ebac08-8531-4296-98c9-8dbc7abcd3f4' },
     ];
     expect(output).to.deep.equal(expected);
   });
