f5636b2f47052f04bdbce89f65f84bdbf7343607
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2020-03-18 12:21:27 +0100

    Add warning about node version not recent enought if there's an error

--- src/command-promise-handler.js
+++ src/command-promise-handler.js
@@ -1,10 +1,16 @@
 'use strict';
 
 const Logger = require('./logger.js');
+const pkg = require('../package.json');
+const semver = require('semver');
 
 function handleCommandPromise (promise) {
   promise.catch((error) => {
     Logger.error(error);
+    const semverIsOk = semver.satisfies(process.version, pkg.engines.node);
+    if (!semverIsOk) {
+      Logger.warn(`You are using node ${process.version}, some of our commands require node ${pkg.engines.node}. The error may be caused by this.`);
+    }
     process.exit(1);
   });
 }
--- src/command-stream-handler.js
+++ src/command-stream-handler.js
@@ -2,11 +2,17 @@
 
 const _ = require('lodash');
 const Logger = require('./logger.js');
+const pkg = require('../package.json');
+const semver = require('semver');
 
 function handleCommandStream (stream) {
   stream.onValue(_.noop);
   stream.onError((error) => {
     Logger.error(error);
+    const semverIsOk = semver.satisfies(process.version, pkg.engines.node);
+    if (!semverIsOk) {
+      Logger.warn(`You are using node ${process.version}, some of our commands require node ${pkg.engines.node}. The error may be caused by this.`);
+    }
     process.exit(1);
   });
   // explicit exit FTW
