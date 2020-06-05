32fc98fa5c7ccc59306a18354f5301c02fc75ce1
Jean-Baptiste Noblot <jean-baptiste@grytics.com>
2020-06-05 14:10:58 +0200

    revert delete error and log it

--- src/models/variables.js
+++ src/models/variables.js
@@ -40,7 +40,7 @@ function parseFromJson (rawStdin) {
     variables = JSON.parse(rawStdin);
   }
   catch (e) {
-    throw new Error('Error when parsing JSON input');
+    throw new Error(`Error when parsing JSON input : ${e.message}`);
   }
 
   if (!Array.isArray(variables) || variables.some((entry) => typeof entry !== 'object')) {
