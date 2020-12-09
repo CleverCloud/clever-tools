765b2205499c94e00f180660acf6f34567b886a1
Emmanuel Feller <emmanuel.feller@gmail.com>
2020-12-09 14:58:10 +0100

    Prettier network group list

--- src/commands/networkgroups.js
+++ src/commands/networkgroups.js
@@ -2,10 +2,29 @@
 
 const networkgroup = require('@clevercloud/client/cjs/api/v4/networkgroup.js');
 
+const formatTable = require('../format-table');
+
 const AppConfig = require('../models/app_configuration.js');
 const Logger = require('../logger.js');
 
 const { sendToApi } = require('../models/send-to-api.js');
+
+// We use examples of maximum width text to have a clean display
+const formatNetworkgroupsTable = formatTable([
+  40, /* id length */
+  20, /* label length */
+  40, /* description */
+]);
+
+function formatNetworkgroupsLine (ng) {
+  return formatNetworkgroupsTable([
+    [
+      ng.id,
+      ng.label,
+      ng.description || ' ',
+    ],
+  ]);
+};
 
 async function listNetworkgroups (params) {
   const { alias, json } = params.options;
@@ -15,9 +34,17 @@ async function listNetworkgroups (params) {
 
   if (json) {
     Logger.println(JSON.stringify(result, null, 2));
-  } else {
-    for (const ng of result) {
-      Logger.println(ng.id, '|', ng.label, '|', ng.description);
+  }
+  else {
+    const resultToPrint = result.map((ng) => formatNetworkgroupsLine(ng));
+    Logger.println(formatNetworkgroupsLine({ id: 'Networkgroup Id', label: 'Label', description: 'Description' }));
+    if (resultToPrint.length === 0) {
+      Logger.println(' No networkgroups found');
+    }
+    else {
+      for (const ng of resultToPrint) {
+        Logger.println(ng);
+      }
     }
   }
 }
@@ -31,7 +58,8 @@ async function createNg (params) {
 
   if (json) {
     Logger.println(JSON.stringify(result, null, 2));
-  } else {
+  }
+  else {
     Logger.println('Networkgroup is created with this id:', result.id);
   }
 }
@@ -40,7 +68,7 @@ async function deleteNg (params) {
   const { alias, 'ng-id': ng_id } = params.options;
   const { ownerId } = await AppConfig.getAppDetails({ alias });
   Logger.debug(`Get networkgroups for the ownerId: ${ownerId}`);
-  const result = await networkgroup.deleteNg({ owner_id: ownerId, id: ng_id }).then(sendToApi);
+  await networkgroup.deleteNg({ owner_id: ownerId, id: ng_id }).then(sendToApi);
 
   Logger.println('Networkgroup is deleted');
 }
@@ -53,12 +81,10 @@ async function listMembers (params) {
 
   if (json) {
     Logger.println(JSON.stringify(result, null, 2));
-  } else {
+  }
+  else {
     Logger.println('Networkgroup contains theses members :');
-    Logger.println("id | member type | label | domain name");
-    for (const member of result) {
-      Logger.println(member.id, '|', member.type, '|', member.label, '|', member['domain-name']);
-    }
+    Logger.println('id | member type | label | domain name');
   }
 }
 
@@ -70,26 +96,27 @@ async function getMember (params) {
 
   if (json) {
     Logger.println(JSON.stringify(result, null, 2));
-  } else {
-    Logger.println("id | member type | label | domain name");
+  }
+  else {
+    Logger.println('id | member type | label | domain name');
     Logger.println(result.id, '|', result.type, '|', result.label, '|', result['domain-name']);
   }
 }
 
 async function addMember (params) {
   const { alias, 'ng-id': ng_id, 'app-id': app_id } = params.options;
-  const { ownerId, appId } = await AppConfig.getAppDetails({ alias });
+  const { ownerId } = await AppConfig.getAppDetails({ alias });
 
-  const result = await networkgroup.addMember({ owner_id: ownerId, id: ng_id }, { id: app_id, label, 'domain-name': domainName, 'type': mtype }).then(sendToApi);
+  await networkgroup.addMember({ owner_id: ownerId, id: ng_id }, { id: app_id, label, 'domain-name': domainName, type: mtype }).then(sendToApi);
 
   Logger.println('add member : OK');
 }
 
 async function removeMember (params) {
   const { alias, 'ng-id': ng_id, 'member-id': memberId } = params.options;
-  const { ownerId, appId } = await AppConfig.getAppDetails({ alias });
+  const { ownerId } = await AppConfig.getAppDetails({ alias });
 
-  const result = await networkgroup.removeMember({ owner_id: ownerId, ngId: ng_id, memberId }).then(sendToApi);
+  await networkgroup.removeMember({ owner_id: ownerId, ngId: ng_id, memberId }).then(sendToApi);
 
   Logger.println('remove member : OK');
 }
@@ -102,9 +129,10 @@ async function listPeers (params) {
 
   if (json) {
     Logger.println(JSON.stringify(result, null, 2));
-  } else {
+  }
+  else {
     Logger.println('Networkgroup contains theses peers :');
-  //  Logger.println("id | member type | label | domain name");
+    //  Logger.println("id | member type | label | domain name");
     for (const peer of result) {
       const ip = (peer.endpoint.type === 'ServerEndpoint') ? peer.endpoint['ng-term'].ip : peer.endpoint['ng-ip'];
       Logger.println(peer.id, '|', peer.type, '|', peer.endpoint.type, '|', peer.label, '|', peer.hostname, '|', ip);
@@ -120,7 +148,8 @@ async function getPeer (params) {
 
   if (json) {
     Logger.println(JSON.stringify(peer, null, 2));
-  } else {
+  }
+  else {
     const ip = (peer.endpoint.type === 'ServerEndpoint') ? peer.endpoint['ng-term'].ip : peer.endpoint['ng-ip'];
     Logger.println(peer.id, '|', peer.type, '|', peer.endpoint.type, '|', peer.label, '|', peer.hostname, '|', ip);
   }
@@ -128,9 +157,9 @@ async function getPeer (params) {
 
 async function removeExternalPeer (params) {
   const { alias, 'ng-id': ng_id, 'peer-id': peerId } = params.options;
-  const { ownerId, appId } = await AppConfig.getAppDetails({ alias });
+  const { ownerId } = await AppConfig.getAppDetails({ alias });
 
-  const result = await networkgroup.removeExternalPeer({ owner_id: ownerId, ngId: ng_id, peerId }).then(sendToApi);
+  await networkgroup.removeExternalPeer({ owner_id: ownerId, ngId: ng_id, peerId }).then(sendToApi);
 
   Logger.println('remove external peer : OK');
 }
