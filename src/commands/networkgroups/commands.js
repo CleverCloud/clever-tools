161420cd1e53bf1de131c7384c13a3431cbb1b0f
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-02-19 12:01:57 +0100

    Refactor import statements

--- src/commands/networkgroups/commands.js
+++ src/commands/networkgroups/commands.js
@@ -1,22 +1,8 @@
 'use strict';
 
-const { listNetworkgroups, createNg, deleteNg } = require('./index.js');
-const { listMembers, getMember, addMember, removeMember } = require('./members.js');
-const { listPeers, getPeer, addExternalPeer, removeExternalPeer } = require('./peers.js');
-const { joinNg, leaveNg } = require('./join.js');
-
 module.exports = {
-  listNetworkgroups,
-  createNg,
-  deleteNg,
-  joinNg,
-  leaveNg,
-  listMembers,
-  getMember,
-  addMember,
-  removeMember,
-  listPeers,
-  getPeer,
-  addExternalPeer,
-  removeExternalPeer,
+  ...require('./index.js'),
+  ...require('./join.js'),
+  ...require('./members.js'),
+  ...require('./peers.js'),
 };
