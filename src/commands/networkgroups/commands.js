a0068543e5abb8962a51373de4e216738e1868ae
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-02-19 11:43:20 +0100

    Refactor members-related commands out of networkgroup commands definition file

--- src/commands/networkgroups/commands.js
+++ src/commands/networkgroups/commands.js
@@ -19,6 +19,7 @@ const TableFormatter = require('./format-table.js');
 const WgConf = require('./wireguard-conf.js');
 
 const { listNetworkgroups, createNg, deleteNg } = require('./index.js');
+const { listMembers, getMember, addMember, removeMember } = require('./members.js');
 
 const { sendToApi, getHostAndTokens } = require('../../models/send-to-api.js');
 
@@ -345,69 +346,6 @@ async function leaveNg (params) {
   // We need `force: true` to avoid errors if file doesn't exist
   fs.rmSync(confPath, { force: true });
   Logger.info(`Deleted WireGuard® configuration file for ${Formatter.formatString(ngId)}`);
-}
-
-async function listMembers (params) {
-  const { ng: ngIdOrLabel, 'natural-name': naturalName, json } = params.options;
-  const ownerId = await Networkgroup.getOwnerId();
-  const ngId = await Networkgroup.getId(ownerId, ngIdOrLabel);
-
-  Logger.info(`Listing members from networkgroup '${ngId}'`);
-  const result = await networkgroup.listMembers({ ownerId, ngId }).then(sendToApi);
-
-  if (json) {
-    Logger.println(JSON.stringify(result, null, 2));
-  }
-  else {
-    if (result.length === 0) {
-      Logger.println(`No member found. You can add one with ${Formatter.formatCommand('clever networkgroups members add')}.`);
-    }
-    else {
-      await TableFormatter.printMembersTableHeader(naturalName);
-      result.forEach(async (ng) => {
-        Logger.println(await TableFormatter.formatMembersLine(ng, naturalName));
-      });
-    }
-  }
-}
-
-async function getMember (params) {
-  const { ng: ngIdOrLabel, 'member-id': memberId, 'natural-name': naturalName, json } = params.options;
-  const ownerId = await Networkgroup.getOwnerId();
-  const ngId = await Networkgroup.getId(ownerId, ngIdOrLabel);
-
-  Logger.info(`Getting details for member ${Formatter.formatString(memberId)} in networkgroup ${Formatter.formatString(ngId)}`);
-  const result = await networkgroup.getMember({ ownerId, ngId, memberId }).then(sendToApi);
-
-  if (json) {
-    Logger.println(JSON.stringify(result, null, 2));
-  }
-  else {
-    await TableFormatter.printMembersTableHeader(naturalName);
-    Logger.println(await TableFormatter.formatMembersLine(result, naturalName));
-  }
-}
-
-async function addMember (params) {
-  const { ng: ngIdOrLabel, 'member-id': memberId, type, 'domain-name': domainName, label } = params.options;
-  const ownerId = await Networkgroup.getOwnerId();
-  const ngId = await Networkgroup.getId(ownerId, ngIdOrLabel);
-
-  const body = { id: memberId, label, 'domain-name': domainName, type };
-  Logger.debug('Sending body: ' + JSON.stringify(body, null, 2));
-  await networkgroup.addMember({ ownerId, ngId }, body).then(sendToApi);
-
-  Logger.println(`Successfully added member ${Formatter.formatString(memberId)} to networkgroup ${Formatter.formatString(ngId)}.`);
-}
-
-async function removeMember (params) {
-  const { ng: ngIdOrLabel, 'member-id': memberId } = params.options;
-  const ownerId = await Networkgroup.getOwnerId();
-  const ngId = await Networkgroup.getId(ownerId, ngIdOrLabel);
-
-  await networkgroup.removeMember({ ownerId, ngId, memberId }).then(sendToApi);
-
-  Logger.println(`Successfully removed member ${Formatter.formatString(memberId)} from networkgroup ${Formatter.formatString(ngId)}.`);
 }
 
 async function listPeers (params) {
---
+++ src/commands/networkgroups/members.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const client = require('@clevercloud/client/cjs/api/v4/networkgroup.js');
+
+const { sendToApi } = require('../../models/send-to-api.js');
+
+const Logger = require('../../logger.js');
+const Networkgroup = require('../../models/networkgroup.js');
+const Formatter = require('./format-string.js');
+const TableFormatter = require('./format-table.js');
+
+async function listMembers (params) {
+  const { ng: ngIdOrLabel, 'natural-name': naturalName, json } = params.options;
+  const ownerId = await Networkgroup.getOwnerId();
+  const ngId = await Networkgroup.getId(ownerId, ngIdOrLabel);
+
+  Logger.info(`Listing members from networkgroup '${ngId}'`);
+  const result = await client.listMembers({ ownerId, ngId }).then(sendToApi);
+
+  if (json) {
+    Logger.println(JSON.stringify(result, null, 2));
+  }
+  else {
+    if (result.length === 0) {
+      Logger.println(`No member found. You can add one with ${Formatter.formatCommand('clever networkgroups members add')}.`);
+    }
+    else {
+      await TableFormatter.printMembersTableHeader(naturalName);
+      result.forEach(async (ng) => {
+        Logger.println(await TableFormatter.formatMembersLine(ng, naturalName));
+      });
+    }
+  }
+}
+
+async function getMember (params) {
+  const { ng: ngIdOrLabel, 'member-id': memberId, 'natural-name': naturalName, json } = params.options;
+  const ownerId = await Networkgroup.getOwnerId();
+  const ngId = await Networkgroup.getId(ownerId, ngIdOrLabel);
+
+  Logger.info(`Getting details for member ${Formatter.formatString(memberId)} in networkgroup ${Formatter.formatString(ngId)}`);
+  const result = await client.getMember({ ownerId, ngId, memberId }).then(sendToApi);
+
+  if (json) {
+    Logger.println(JSON.stringify(result, null, 2));
+  }
+  else {
+    await TableFormatter.printMembersTableHeader(naturalName);
+    Logger.println(await TableFormatter.formatMembersLine(result, naturalName));
+  }
+}
+
+async function addMember (params) {
+  const { ng: ngIdOrLabel, 'member-id': memberId, type, 'domain-name': domainName, label } = params.options;
+  const ownerId = await Networkgroup.getOwnerId();
+  const ngId = await Networkgroup.getId(ownerId, ngIdOrLabel);
+
+  const body = { id: memberId, label, 'domain-name': domainName, type };
+  Logger.debug('Sending body: ' + JSON.stringify(body, null, 2));
+  await client.addMember({ ownerId, ngId }, body).then(sendToApi);
+
+  Logger.println(`Successfully added member ${Formatter.formatString(memberId)} to networkgroup ${Formatter.formatString(ngId)}.`);
+}
+
+async function removeMember (params) {
+  const { ng: ngIdOrLabel, 'member-id': memberId } = params.options;
+  const ownerId = await Networkgroup.getOwnerId();
+  const ngId = await Networkgroup.getId(ownerId, ngIdOrLabel);
+
+  await client.removeMember({ ownerId, ngId, memberId }).then(sendToApi);
+
+  Logger.println(`Successfully removed member ${Formatter.formatString(memberId)} from networkgroup ${Formatter.formatString(ngId)}.`);
+}
+
+module.exports = {
+  listMembers,
+  getMember,
+  addMember,
+  removeMember,
+};
