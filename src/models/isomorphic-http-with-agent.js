1bfbf40d388cc78c9b987d3717ecd8b4e1bfe803
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2023-12-05 10:59:02 +0100

    fix: pass an HTTP agent with long timeout to isomorphic-git
    Closes #640

---
+++ src/models/isomorphic-http-with-agent.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const isomotphicHttp = require('isomorphic-git/http/node');
+const https = require('https');
+
+// We use our own HTTP plugin, so we can customize the agent used for requests and configure a long timeout (default is 5 seconds).
+
+const agent = new https.Agent({
+  keepAlive: true,
+  timeout: 10 * 60 * 1000,
+});
+
+function request (params) {
+  return isomotphicHttp.request({ ...params, agent });
+}
+
+module.exports = {
+  request,
+};
