f246e3fd20ee9fa3af449668adcfda817db1e291
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2024-07-25 16:18:58 +0200

    chore: add bundle script (bundle all code into one CJS file)

--- scripts/build.js
+++ scripts/build.js
@@ -1,22 +1,32 @@
 import { exec as pkg } from 'pkg';
 import * as cfg from './config.js';
-import { getBinaryFilepath, getBinaryDirectory } from './paths.js';
-import { startTask, endTask, cleanupDirectory } from './utils.js';
+import { getBinaryFilepath, getBinaryDirectory, getWorkingDirectory } from './paths.js';
+import { startTask, endTask, cleanupDirectory, exec } from './utils.js';
 
 export async function build (version) {
   await cleanupDirectory(getBinaryDirectory(version));
 
+  const singleCjsScriptFilepath = getWorkingDirectory(version) + '/clever.cjs';
+
+  await bundleToSingleCjsScript(singleCjsScriptFilepath);
+
   for (const arch of cfg.archList) {
     const binaryFilepath = getBinaryFilepath(arch, version);
-    await buildBinary(arch, binaryFilepath);
+    await buildBinary(arch, singleCjsScriptFilepath, binaryFilepath);
   }
 };
 
 // --- private
 
-async function buildBinary (arch, binaryFilepath) {
+async function bundleToSingleCjsScript (singleCjsScriptFilepath) {
+  startTask('Bundling project with rollup to single CJS script');
+  await exec(`npm run build -- -o ${singleCjsScriptFilepath}`);
+  endTask(`Bundling project with rollup to single CJS script to ${singleCjsScriptFilepath}`);
+}
+
+async function buildBinary (arch, sourceFilepath, binaryFilepath) {
   const { nodeVersion } = cfg;
   startTask(`Building binary for ${arch}`);
-  await pkg(['.', '-t', `node${nodeVersion}-${arch}`, '-o', binaryFilepath]);
+  await pkg([sourceFilepath, '-t', `node${nodeVersion}-${arch}`, '-o', binaryFilepath]);
   endTask(`Building binary for ${arch} to ${binaryFilepath}`);
 }
