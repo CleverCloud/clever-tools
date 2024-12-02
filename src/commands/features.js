5c370b3bdb0ad99509004491f9360f20c0a36632
David Legrand <david.legrand@clever-cloud.com>
2024-12-02 15:08:17 +0100

    feat(features): introduce `features` command

---
+++ src/commands/features.js
@@ -0,0 +1,93 @@
+import { getFeatures, setFeature } from '../models/configuration.js';
+import { EXPERIMENTAL_FEATURES } from '../experimental-features.js';
+import { formatTable as initFormatTable } from '../format-table.js';
+import { Logger } from '../logger.js';
+
+const formatTable = initFormatTable();
+
+export async function list (params) {
+  const { format } = params.options;
+
+  const featuresConf = await getFeatures();
+  // Add status from configuration file and remove instructions
+  const features = Object.entries(EXPERIMENTAL_FEATURES).map(([id, feature]) => {
+    const enabled = featuresConf[id] === true;
+    return {
+      id,
+      status: feature.status,
+      description: feature.description,
+      enabled,
+    };
+  });
+
+  // For each feature, print the object with the id, status, description and enabled
+  switch (format) {
+    case 'json': {
+      Logger.printJson(features);
+      break;
+    }
+    case 'human':
+    default: {
+      const headers = ['ID', 'STATUS', 'DESCRIPTION', 'ENABLED'];
+
+      Logger.println(formatTable([
+        headers,
+        ...features.map((feature) => [
+          feature.id,
+          feature.status,
+          feature.description,
+          feature.enabled,
+        ]),
+      ]));
+    }
+  }
+}
+
+export async function info (params) {
+  const { feature } = params.namedArgs;
+  const availableFeatures = Object.keys(EXPERIMENTAL_FEATURES);
+
+  if (!availableFeatures.includes(feature)) {
+    throw new Error(`Unavailable feature: ${feature}`);
+  }
+
+  Logger.println(EXPERIMENTAL_FEATURES[feature].instructions);
+}
+
+export async function enable (params) {
+  const { features } = params.namedArgs;
+  const availableFeatures = Object.keys(EXPERIMENTAL_FEATURES);
+
+  const unknownFeatures = features.filter((feature) => !availableFeatures.includes(feature));
+  if (unknownFeatures.length > 0) {
+    throw new Error(`Unavailable feature(s): ${unknownFeatures.join(', ')}`);
+  }
+
+  for (const featureName of features) {
+    await setFeature(featureName, true);
+    Logger.println(`Experimental feature '${featureName}' enabled`);
+  }
+
+  if (features.length === 1) {
+    Logger.println(EXPERIMENTAL_FEATURES[features[0]].instructions);
+  }
+  else {
+    Logger.println();
+    Logger.println("To learn more about these experimental features, use 'clever features info FEATURE_NAME'");
+  }
+}
+
+export async function disable (params) {
+  const { features } = params.namedArgs;
+  const availableFeatures = Object.keys(EXPERIMENTAL_FEATURES);
+
+  const unknownFeatures = features.filter((feature) => !availableFeatures.includes(feature));
+  if (unknownFeatures.length > 0) {
+    throw new Error(`Unavailable feature(s): ${unknownFeatures.join(', ')}`);
+  }
+
+  for (const featureName of features) {
+    await setFeature(featureName, false);
+    Logger.println(`Experimental feature '${featureName}' disabled`);
+  }
+}
