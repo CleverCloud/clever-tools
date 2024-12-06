19a684867a99ee8f416726c05ddf9a38a88a6a75
David Legrand <1110600+davlgd@users.noreply.github.com>
2024-12-06 18:50:17 +0100

    chore: adapt messages to new app deployment/creation

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -2,11 +2,12 @@ import * as Application from '../models/application.js';
 import * as application from '@clevercloud/client/esm/api/v2/application.js';
 import { Logger } from '../logger.js';
 import { sendToApi } from '../models/send-to-api.js';
+import colors from 'colors/safe.js';
 
 export async function stop (params) {
   const { alias, app: appIdOrName } = params.options;
   const { ownerId, appId } = await Application.resolveId(appIdOrName, alias);
 
   await application.undeploy({ id: ownerId, appId }).then(sendToApi);
-  Logger.println('App successfully stopped!');
+  Logger.println(colors.bold.green('✓'), 'Application successfully stopped!');
 }
