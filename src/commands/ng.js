d042386aeeeb103322020e2bbfa949af4dd3f19c
David Legrand <1110600+davlgd@users.noreply.github.com>
2025-01-28 09:22:17 +0100

    feat(ng): add ng commands and tools

---
+++ src/commands/ng.js
@@ -0,0 +1,190 @@
+import colors from 'colors/safe.js';
+import { Logger } from '../logger.js';
+import * as NG from '../models/ng.js';
+import * as NGPrint from './ng-print.js';
+import * as NGResources from '../models/ng-resources.js';
+
+/** Create a Network Group
+ * @param {Object} params
+ * @param {string} params.args[0] Network Group label
+ * @param {string} params.options.description Network Group description
+ * @param {Array<string>}  params.options.link Array of member IDs or labels to link to the Network Group
+ * @param {Object} params.options.org Organisation ID or name
+ * @param {string} params.options.tags Comma-separated list of tags
+ */
+export async function createNg (params) {
+  const label = params.args[0].ngResourceLabel;
+  const { description, link: membersIds, org, tags } = params.options;
+
+  await NG.create(label, description, tags, membersIds, org);
+
+  const membersIdsMessage = membersIds ? ` with member(s):\n${colors.grey(` - ${membersIds.join('\n - ')}`)}` : '';
+  Logger.println(`${colors.bold.green('✓')} Network Group ${colors.green(label)} successfully created${membersIdsMessage}!`);
+}
+
+/** Delete a Network Group
+ * @param {Object} params
+ * @param {Object} params.args[0] Network Group ID or label
+ * @param {Object} params.options.org Organisation ID or name
+ */
+export async function deleteNg (params) {
+  const [ngIdOrLabel] = params.args;
+  const { org } = params.options;
+
+  await NG.destroy(ngIdOrLabel, org);
+  Logger.println(`${colors.bold.green('✓')} Network Group ${colors.green(ngIdOrLabel.ngResourceLabel || ngIdOrLabel.ngId)} successfully deleted!`);
+}
+
+/** Create an external peer in a Network Group
+ * @param {Object} params
+ * @param {Object} params.args[0] External peer ID or label
+ * @param {Object} params.args[1] Network Group ID or label
+ * @param {string} params.args[2] Wireguard public key
+ * @param {Object} params.options.org Organisation ID or name
+ */
+export async function createExternalPeer (params) {
+  const [idOrLabel, ngIdOrLabel, publicKey] = params.args;
+  const { org } = params.options;
+
+  await NGResources.createExternalPeerWithParent(ngIdOrLabel, idOrLabel.ngResourceLabel, publicKey, org);
+  Logger.println(`${colors.bold.green('✓')} External peer ${colors.green(idOrLabel.ngResourceLabel)} successfully created in Network Group ${colors.green(ngIdOrLabel.ngResourceLabel || ngIdOrLabel.ngId)}`);
+}
+
+/** Delete an external peer from a Network Group
+ * @param {Object} params
+ * @param {Object} params.args[0] External peer ID or label
+ * @param {Object} params.args[1] Network Group ID or label
+ * @param {Object} params.options.org Organisation ID or name
+ */
+export async function deleteExternalPeer (params) {
+  const [idOrLabel, ngIdOrLabel] = params.args;
+  const { org } = params.options;
+
+  await NGResources.deleteExternalPeerWithParent(ngIdOrLabel, idOrLabel.ngResourceLabel || idOrLabel.memberId, org);
+  Logger.println(`${colors.bold.green('✓')} External peer ${colors.green(idOrLabel.ngResourceLabel || idOrLabel.memberId)} successfully deleted from Network Group ${colors.green(ngIdOrLabel.ngResourceLabel || ngIdOrLabel.ngId)}`);
+}
+
+/** Link a member to a Network Group
+ * @param {Object} params
+ * @param {string} params.args[0] Member ID
+ * @param {Object} params.args[1] Network Group ID or label
+ * @param {Object} params.options.org Organisation ID or name
+ */
+export async function linkToNg (params) {
+  const [resourceId, ngIdOrLabel] = params.args;
+  const { org } = params.options;
+
+  await NGResources.linkMember(ngIdOrLabel, resourceId.memberId, org);
+  Logger.println(`${colors.bold.green('✓')} Member ${colors.green(resourceId.memberId)} successfully linked to Network Group ${colors.green(ngIdOrLabel.ngResourceLabel || ngIdOrLabel.ngId)}`);
+}
+
+/** Unlink a member from a Network Group
+ * @param {Object} params
+ * @param {string} params.args[0] Member ID
+ * @param {Object} params.args[1] Network Group ID or label
+ * @param {Object} params.options.org Organisation ID or name
+ */
+export async function unlinkFromNg (params) {
+  const [resourceId, ngIdOrLabel] = params.args;
+  const { org } = params.options;
+
+  await NGResources.unlinkMember(ngIdOrLabel, resourceId.memberId, org);
+  Logger.println(`${colors.bold.green('✓')} Member ${colors.green(resourceId.memberId)} successfully unlinked from Network Group ${colors.green(ngIdOrLabel.ngResourceLabel || ngIdOrLabel.ngId)}`);
+}
+
+/** Print the configuration of a Network Group's peer
+ * @param {Object} params
+ * @param {Object} params.args[0] Peer ID or label
+ * @param {Object} params.args[1] Network Group ID or label
+ * @param {Object} params.options.org Organisation ID or name
+ * @param {string} params.options.format Output format
+ */
+export async function getPeerConfig (params) {
+  const [peerIdOrLabel, ngIdOrLabel] = params.args;
+  const { org, format } = params.options;
+
+  const config = await NG.getPeerConfig(peerIdOrLabel, ngIdOrLabel, org);
+
+  switch (format) {
+    case 'json': {
+      Logger.printJson(config);
+      break;
+    }
+    case 'human':
+    default: {
+      const decodedConfiguration = Buffer.from(config.configuration, 'base64').toString('utf8');
+      Logger.println(decodedConfiguration);
+    }
+  }
+}
+
+/** List Network Groups, their members and peers
+ * @param {Object} params
+ * @param {Object} params.options.orgaIdOrName Organisation ID or name
+ * @param {string} params.options.format Output format
+ */
+export async function listNg (params) {
+  const { org, format } = params.options;
+
+  const ngs = await NG.getAllNGs(org);
+
+  if (!ngs.length) {
+    Logger.println(`ℹ️ No Network Group found, create one with ${colors.blue('clever ng create')} command`);
+    return;
+  }
+
+  switch (format) {
+    case 'json': {
+      Logger.printJson(ngs);
+      break;
+    }
+    case 'human':
+    default: {
+      const ngList = ngs.map(({
+        id,
+        label,
+        networkIp,
+        members,
+        peers,
+      }) => ({
+        ID: id,
+        Label: label,
+        'Network CIDR': networkIp,
+        Members: Object.keys(members).length,
+        Peers: Object.keys(peers).length,
+      }));
+
+      console.table(ngList);
+    }
+  }
+}
+
+/** Show information about a Network Group, a member or a peer
+ * @param {Object} params
+ * @param {Object} params.args[0] ID or label of the Network Group, a member or a peer
+ * @param {Object} params.options.org Organisation ID or name
+ * @param {string} params.options.format Output format
+ */
+export async function get (params) {
+  const [idOrLabel] = params.args;
+  const { org, format } = params.options;
+  let type = params.options.type;
+
+  if (!type) type = 'single';
+
+  NGPrint.results(idOrLabel, org, format, 'get', type);
+}
+
+/** Show information about a Network Group, a member or a peer
+ * @param {Object} params
+ * @param {Object} params.args[0] ID or label of the Network Group, a member or a peer
+ * @param {Object} params.options.org Organisation ID or name
+ * @param {string} params.options.format Output format
+ */
+export async function search (params) {
+  const [idOrLabel] = params.args;
+  const { org, format } = params.options;
+  const type = params.options.type;
+
+  NGPrint.results(idOrLabel, org, format, 'search', type);
+}
---
+++ src/models/ng.js
@@ -0,0 +1,271 @@
+import colors from 'colors/safe.js';
+import * as User from '../models/user.js';
+import * as Organisation from '../models/organisation.js';
+import * as ngApi from '@clevercloud/client/cjs/api/v4/network-group.js';
+
+import { searchNetworkGroupOrResource } from './ng-api.js';
+import { checkMembersToLink } from './ng-resources.js';
+import { sendToApi } from './send-to-api.js';
+import { Logger } from '../logger.js';
+import { v4 as uuidv4 } from 'uuid';
+
+export const TIMEOUT = 30;
+export const INTERVAL = 1000;
+export const DOMAIN = 'ng-cc.cloud';
+export const TYPE_PREFIXES = {
+  app_: 'APPLICATION',
+  addon_: 'ADDON',
+  external_: 'EXTERNAL',
+};
+
+/**
+ * Ask for a Network Group creation
+ * @param {string} label The Network Group label
+ * @param {string} description The Network Group description
+ * @param {string} tags The Network Group tags
+ * @param {Array<string>} membersIds The members to link to the Network Group
+ * @param {string} orgaIdOrName The owner ID or name
+ * @throws {Error} If the Network Group label is missing
+ */
+export async function create (label, description, tags, membersIds, orgaIdOrName) {
+  if (!label) {
+    throw new Error('A valid Network Group label is required');
+  }
+
+  await checkMembersToLink(membersIds);
+
+  const id = `ng_${uuidv4()}`;
+  const ownerId = await getOwnerIdFromOrgaIdOrName(orgaIdOrName);
+
+  const members = constructMembers(id, membersIds || []);
+  const body = { ownerId, id, label, description, tags, members };
+
+  Logger.info(`Creating Network Group ${label} (${id}) from owner ${ownerId}`);
+  Logger.info(`${members.length} members will be added: ${members.map((m) => m.id).join(', ')}`);
+  Logger.debug(`Sending body: ${JSON.stringify(body, null, 2)}`);
+  await ngApi.createNetworkGroup({ ownerId }, body).then(sendToApi);
+
+  await pollNetworkGroup(ownerId, id, { waitForMembers: membersIds });
+  Logger.info(`Network Group ${label} (${id}) created from owner ${ownerId}`);
+}
+
+/**
+ * Ask for a Network Group deletion
+ * @param {object} ngIdOrLabel The Network Group ID or Label
+ * @param {object} orgaIdOrName The owner ID or name
+ * @throws {Error} If the Network Group is not found
+ */
+export async function destroy (ngIdOrLabel, orgaIdOrName) {
+  const [found] = await searchNgOrResource(ngIdOrLabel, orgaIdOrName, 'NetworkGroup');
+
+  if (!found) {
+    throw new Error(`Network Group ${colors.red(ngIdOrLabel.ngId || ngIdOrLabel.ngResourceLabel)} not found`);
+  }
+
+  await ngApi.deleteNetworkGroup({ ownerId: found.ownerId, networkGroupId: found.id }).then(sendToApi);
+  Logger.info(`Deleting Network Group ${found.id} from owner ${found.ownerId}`);
+  await pollNetworkGroup(found.ownerId, found.id, { waitForDeletion: true });
+  Logger.info(`Network Group ${found.id} deleted from owner ${found.ownerId}`);
+}
+
+/**
+ * Get the Wireguard configuration of a Network Group peer
+ * @param {object} peerIdOrLabel The Peer ID or Label
+ * @param {object} ngIdOrLabel The Network Group ID or Label
+ * @param {object} orgaIdOrName The owner ID or name
+ * @returns {Promise<Object>} The Peer Wireguard configuration
+ * @throws {Error} If the Peer is not found
+ * @throws {Error} If the Network Group is not found
+ * @throws {Error} If the Peer is not in the Network Group
+ */
+export async function getPeerConfig (peerIdOrLabel, ngIdOrLabel, orgaIdOrName) {
+  const [peer] = await searchNgOrResource(peerIdOrLabel, orgaIdOrName, 'Peer');
+
+  if (!peer || (peerIdOrLabel.ngResourceLabel && peer.label !== peerIdOrLabel.ngResourceLabel)) {
+    throw new Error(`Peer ${colors.red(peerIdOrLabel.ngResourceLabel || peerIdOrLabel.member)} not found`);
+  }
+
+  const [parentNg] = await searchNgOrResource(ngIdOrLabel, orgaIdOrName, 'NetworkGroup');
+
+  if (!parentNg) {
+    throw new Error(`Network Group ${colors.red(ngIdOrLabel.ngId || ngIdOrLabel.ngResourceLabel)} not found`);
+  }
+
+  if (!parentNg.peers[peer.id]) {
+    throw new Error(`Peer ${colors.red(peer.id)} is not in Network Group ${colors.red(parentNg.id)}`);
+  }
+
+  Logger.debug(`Getting configuration for Peer ${peer.id}`);
+  const result = await ngApi.getNetworkGroupWireGuardConfiguration({
+    ownerId: parentNg.ownerId,
+    networkGroupId: parentNg.id,
+    peerId: peer.id,
+  }).then(sendToApi);
+  Logger.debug(`Received from API:\n${JSON.stringify(result, null, 2)}`);
+
+  return result;
+}
+
+/**
+ * Get a Network group from an owner with members and peers
+ * @param {object} ngIdOrLabel The Network Group ID or Label
+ * @param {string} orgaIdOrName The owner ID or name
+ * @returns {Promise<Array<Object>>} The Network Groups
+ */
+export async function getNG (networkGroupId, orgaIdOrName) {
+  const ownerId = await getOwnerIdFromOrgaIdOrName(orgaIdOrName);
+
+  Logger.info(`Get Network Group ${networkGroupId} for owner ${ownerId}`);
+  const result = await ngApi.getNetworkGroup({ networkGroupId, ownerId }).then(sendToApi);
+  Logger.debug(`Received from API:\n${JSON.stringify(result, null, 2)}`);
+
+  return result;
+}
+
+/**
+ * Get all Network Groups from an owner with members and peers
+ * @param {string} orgaIdOrName The owner ID or name
+ * @returns {Promise<Array<Object>>} The Network Groups
+ */
+export async function getAllNGs (orgaIdOrName) {
+  const ownerId = await getOwnerIdFromOrgaIdOrName(orgaIdOrName);
+
+  Logger.info(`Listing Network Groups from owner ${ownerId}`);
+  const result = await ngApi.listNetworkGroups({ ownerId }).then(sendToApi);
+  Logger.debug(`Received from API:\n${JSON.stringify(result, null, 2)}`);
+  return result;
+}
+
+/**
+ * Search a Network Group or a resource (member/peer)
+ * @param {string|Object} idOrLabel The ID or label to look for
+ * @param {Object} orgaIdOrName The owner ID or name
+ * @param {boolean} type Look only for a specific type (NetworkGroup, Member, CleverPeer, ExternalPeer, Peer), can be 'single', default to 'all'
+ * @param {boolean} exactMatch Look for exact match, default to true
+ * @throws {Error} If multiple Network Groups or member/peer are found in single_result mode
+ * @returns {Promise<Object>} Found results
+ */
+export async function searchNgOrResource (idOrLabel, orgaIdOrName, type = 'all', exactMatch = true) {
+  const ownerId = await getOwnerIdFromOrgaIdOrName(orgaIdOrName);
+
+  // If idOrLabel is a string we use it, or we look through multiple keys
+  const query = typeof idOrLabel === 'string'
+    ? idOrLabel
+    : (
+        idOrLabel.ngId
+        || idOrLabel.memberId
+        || idOrLabel.ngResourceLabel
+      );
+
+  const found = await searchNetworkGroupOrResource({ ownerId, query }).then(sendToApi);
+
+  let filtered = found;
+  switch (type) {
+    case 'all':
+    case 'single':
+      break;
+    case 'Peer':
+      filtered = found.filter((f) => f.type === 'CleverPeer' || f.type === 'ExternalPeer');
+      break;
+    case 'CleverPeer':
+    case 'ExternalPeer':
+    case 'Member':
+    case 'NetworkGroup':
+      filtered = found.filter((f) => f.type === type);
+      break;
+    default:
+      throw new Error(`Unsupported type: ${type}`);
+  }
+
+  if (exactMatch) {
+    filtered = filtered.filter((f) => f.id === query || f.label === query);
+  }
+
+  if (filtered.length > 1 && !type === 'all') {
+    throw new Error(`Multiple resources found for ${colors.red(query)}, use ID instead:
+${filtered.map((f) => ` - ${f.id} ${colors.grey(`(${f.label} - ${f.type})`)}`).join('\n')}`);
+  }
+
+  // Deduplicate results
+  return filtered.filter((item, index, array) => array.findIndex((element) => (element.id === item.id)) === index);
+}
+
+/**
+ * Construct members from members_ids
+ * @param {string} ngId The Network Group ID
+ * @param {Array<string>} members_ids The members IDs
+ * @returns {Array<Object>} Array of members with id, domainName and kind
+ */
+export function constructMembers (ngId, membersIds) {
+  return membersIds.map((id) => {
+    const domainName = `${id}.m.${ngId}.${DOMAIN}`;
+    const prefixToType = TYPE_PREFIXES;
+
+    return {
+      id,
+      domainName,
+      // Get kind from prefix match in id (app_*, addon_*, external_*) or default to 'APPLICATION'
+      kind: prefixToType[Object.keys(prefixToType).find((p) => id.startsWith(p))]
+        || TYPE_PREFIXES.app_,
+    };
+  });
+}
+
+/**
+ * Poll Network Groups to check its status and members
+ * @param {string} ownerId The owner ID
+ * @param {string} ngId The Network Group ID
+ * @param {Array<string>} waitForMembers The members IDs to wait for
+ * @param {boolean} waitForDeletion Wait for the Network Group deletion
+ * @throws {Error} When timeout is reached
+ * @returns {Promise<void>}
+ */
+async function pollNetworkGroup (ownerId, ngId, { waitForMembers = null, waitForDeletion = false } = {}) {
+  return new Promise((resolve, reject) => {
+    Logger.info(`Polling Network Groups from owner ${ownerId}`);
+
+    const poll = setInterval(async () => {
+      // We don't use ngApi.getNetworkGroup(), it will lead to an error before creation
+      const ngs = await ngApi.listNetworkGroups({ ownerId }).then(sendToApi);
+      const ng = ngs.find((ng) => ng.id === ngId);
+
+      if (waitForDeletion && !ng) {
+        cleanup(true);
+        return;
+      }
+
+      if (!waitForDeletion && ng) {
+        if (waitForMembers?.length) {
+          const members = ng.members.filter((member) => waitForMembers.includes(member.id));
+          if (members.length !== waitForMembers.length) {
+            Logger.debug(`Waiting for members: ${waitForMembers.join(', ')}`);
+            return;
+          }
+        }
+        cleanup(true);
+      }
+    }, INTERVAL);
+
+    const timer = setTimeout(() => {
+      const action = waitForDeletion ? 'deletion of' : 'creation of';
+      cleanup(false, new Error(`Timeout while checking ${action} Network Group ${ngId}`));
+    }, TIMEOUT * 1000);
+
+    function cleanup (success, error = null) {
+      clearInterval(poll);
+      clearTimeout(timer);
+      success ? resolve() : reject(error);
+    }
+  });
+}
+
+/**
+ * Get the owner ID from an Organisation ID or name
+ * @param {object} orgaIdOrName The Organisation ID or name
+ * @returns {Promise<string>} The owner ID
+ */
+async function getOwnerIdFromOrgaIdOrName (orgaIdOrName) {
+  return orgaIdOrName != null
+    ? Organisation.getId(orgaIdOrName)
+    : User.getCurrentId();
+}
