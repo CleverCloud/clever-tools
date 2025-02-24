6a9cb08045f523f03bcfd1b0313b59835906b47b
David Legrand <1110600+davlgd@users.noreply.github.com>
2025-02-24 20:07:33 +0100

    chore: use dedent for experimental features

--- src/experimental-features.js
+++ src/experimental-features.js
@@ -1,8 +1,10 @@
+import dedent from 'dedent';
+
 export const EXPERIMENTAL_FEATURES = {
   kv: {
     status: 'alpha',
     description: 'Send commands to databases such as Materia KV or Redis® directly from Clever Tools, without other dependencies',
-    instructions: `
+    instructions: dedent(`
       Target any compatible add-on by its name or ID (with an org ID if needed) and send commands to it:
 
           clever kv myMateriaKV SET myKey myValue
@@ -12,6 +14,6 @@ export const EXPERIMENTAL_FEATURES = {
           clever kv redis_xxxxx --org org_xxxxx PING
 
       Learn more about Materia KV: https://www.clever-cloud.com/developers/doc/addons/materia-kv/
-      `,
+      `),
   },
 };
