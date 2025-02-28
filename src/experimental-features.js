fe988179cc208f2dd1e8d7dda84774aa269c7282
David Legrand <david.legrand@clever-cloud.com>
2025-02-28 21:07:40 +0100

    feat: add keycloak command

--- src/experimental-features.js
+++ src/experimental-features.js
@@ -63,5 +63,9 @@ export const EXPERIMENTAL_FEATURES = {
     description: 'Manage operators with dedicated features such as Keycloak, Matomo, Metabase, Otoroshi',
     instructions: `
 `,
+clever keycloak
+clever keycloak get keycloak_xxx
+clever keycloak ng enable myKeycloak
+
   },
 };
