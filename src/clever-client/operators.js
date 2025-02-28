fa94a8aabb3240efa80e8c4924173f5061d14376
David Legrand <david.legrand@clever-cloud.com>
2025-02-28 21:01:45 +0100

    chore: add Operators API support

---
+++ src/clever-client/operators.js
@@ -0,0 +1,86 @@
+// TODO: Move this to the Clever Cloud JS Client
+
+/**
+ * GET /v4/addon-providers/addon-{provider}/addons/{realId}
+ * @param {Object} params
+ * @param {String} params.undefined
+ * @param {String} params.undefined
+ */
+export function getOperator (params) {
+  // no multipath for /self or /organisations/{id}
+  return Promise.resolve({
+    method: 'get',
+    url: `/v4/addon-providers/addon-${params.provider}/addons/${params.realId}`,
+    headers: { Accept: 'application/json' },
+    // no queryParams
+    // no body
+  });
+}
+
+/**
+ * POST /v4/addon-providers/addon-{provider}/addons/{realId}/reboot
+ * @param {Object} params
+ * @param {String} params.undefined
+ * @param {String} params.undefined
+ */
+export function rebootOperator (params) {
+  // no multipath for /self or /organisations/{id}
+  return Promise.resolve({
+    method: 'post',
+    url: `/v4/addon-providers/addon-${params.provider}/addons/${params.realId}/reboot`,
+    headers: { Accept: 'application/json' },
+    // no queryParams
+    // no body
+  });
+}
+
+/**
+ * POST /v4/addon-providers/addon-{provider}/addons/{realId}/rebuild
+ * @param {Object} params
+ * @param {String} params.undefined
+ * @param {String} params.undefined
+ */
+export function rebuildOperator (params) {
+  // no multipath for /self or /organisations/{id}
+  return Promise.resolve({
+    method: 'post',
+    url: `/v4/addon-providers/addon-${params.provider}/addons/${params.realId}/rebuild`,
+    headers: { Accept: 'application/json' },
+    // no queryParams
+    // no body
+  });
+}
+
+/**
+ * POST /v4/addon-providers/addon-{provider}/addons/{realId}/ng
+ * @param {Object} params
+ * @param {String} params.undefined
+ * @param {String} params.undefined
+ */
+export function ngEnableOperator (params) {
+  // no multipath for /self or /organisations/{id}
+  return Promise.resolve({
+    method: 'post',
+    url: `/v4/addon-providers/addon-${params.provider}/addons/${params.realId}/ng`,
+    headers: { Accept: 'application/json' },
+    // no queryParams
+    // no body
+  });
+}
+
+/**
+ * POST /v4/addon-providers/addon-{provider}/addons/{realId}/ng
+ * @param {Object} params
+ * @param {String} params.undefined
+ * @param {String} params.undefined
+ */
+export function ngDisableOperator (params) {
+  // no multipath for /self or /organisations/{id}
+  return Promise.resolve({
+    method: 'delete',
+    url: `/v4/addon-providers/addon-${params.provider}/addons/${params.realId}/ng`,
+    headers: { Accept: 'application/json' },
+    // no queryParams
+    // no body
+  });
+}
