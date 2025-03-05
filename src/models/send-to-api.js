379c8667c2c4b74e2e0edbec547f2ff0b3657e97
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2025-03-05 23:57:53 +0100

    feat: display a proper message for any 401 from the API

--- src/models/send-to-api.js
+++ src/models/send-to-api.js
@@ -4,6 +4,8 @@ import { conf, loadOAuthConf } from './configuration.js';
 import { prefixUrl } from '@clevercloud/client/esm/prefix-url.js';
 import { request } from '@clevercloud/client/esm/request.fetch.js';
 import { subtle as cryptoSuble } from 'node:crypto';
+import { addOauthHeaderPlaintext } from '../clever-client/auth-bridge.js';
+import colors from 'colors/safe.js';
 
 // Required for @clevercloud/client with "old" Node.js
 if (globalThis.crypto == null) {
@@ -35,6 +37,19 @@ export async function sendToApi (requestParams) {
     .catch(processError);
 }
 
+export async function sendToAuthBridge (requestParams) {
+  const tokens = await loadTokens();
+  return Promise.resolve(requestParams)
+    .then(prefixUrl(conf.AUTH_BRIDGE_HOST))
+    .then(addOauthHeaderPlaintext(tokens))
+    .then((requestParams) => {
+      Logger.debug(`${requestParams.method.toUpperCase()} ${requestParams.url} ? ${JSON.stringify(requestParams.queryParams)}`);
+      return requestParams;
+    })
+    .then(request)
+    .catch(processError);
+}
+
 export function processError (error) {
   const code = error.code ?? error?.cause?.code;
   if (code === 'EAI_AGAIN') {
@@ -42,6 +57,9 @@ export function processError (error) {
   }
   if (code === 'ECONNRESET') {
     throw new Error('The connection to the Clever Cloud API was closed abruptly, please try again.', { cause: error });
+  }
+  if (error?.response?.status === 401) {
+    throw new Error(`You're not logged in, use ${colors.red('clever login')} command to connect to your Clever Cloud account`, { cause: error });
   }
   throw error;
 }
