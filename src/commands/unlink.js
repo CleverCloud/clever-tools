3b27d74128545df3abefb4b7cd115ad0c7a47675
David Legrand <david.legrand@clever-cloud.com>
2025-06-08 11:42:25 +0200

    feat(unlink): improve display (texts and colors)

--- src/commands/unlink.js
+++ src/commands/unlink.js
@@ -1,11 +1,12 @@
 import * as AppConfig from '../models/app_configuration.js';
 import * as Application from '../models/application.js';
 import { Logger } from '../logger.js';
+import colors from 'colors/safe.js';
 
 export async function unlink (params) {
   const [alias] = params.args;
   const app = await AppConfig.getAppDetails({ alias });
 
   await Application.unlinkRepo(app.alias);
-  Logger.println('Your application has been successfully unlinked!');
+  Logger.printSuccess(`Application ${colors.green(app.appId)} has been successfully unlinked from local alias ${colors.green(app.alias)}!`);
 };
