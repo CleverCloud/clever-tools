7ac000ca1cdbc4d1dfed094958eea9feb8e5b286
David Legrand <david.legrand@clever-cloud.com>
2025-06-08 11:46:20 +0200

    fix(delete): exit with error if app doesn't exist

--- src/commands/delete.js
+++ src/commands/delete.js
@@ -9,17 +9,16 @@ export async function deleteApp (params) {
 
   const app = await Application.get(ownerId, appId);
   if (app == null) {
-    Logger.println('The application doesn\'t exist');
+    throw new Error('The application doesn\'t exist');
   }
-  else {
-    // delete app
-    await Application.deleteApp(app, skipConfirmation);
-    Logger.printSuccess(`Application ${colors.green(colors.bold(`${app.name}`))} successfully deleted!`);
-    Logger.println(`  ${colors.grey('•')} Application ID: ${colors.grey(app.id)}`);
 
-    const wasUnlinked = await AppConfig.removeLinkedApplication({ appId, alias });
-    if (wasUnlinked) {
-      Logger.println(`  ${colors.blue('→')} Local alias ${colors.blue(alias || app.name)} unlinked`);
-    }
+  // delete app
+  await Application.deleteApp(app, skipConfirmation);
+  Logger.printSuccess(`Application ${colors.green(colors.bold(`${app.name}`))} successfully deleted!`);
+  Logger.println(`  ${colors.grey('•')} Application ID: ${colors.grey(app.id)}`);
+
+  const wasUnlinked = await AppConfig.removeLinkedApplication({ appId, alias });
+  if (wasUnlinked) {
+    Logger.println(`  ${colors.blue('→')} Local alias ${colors.blue(alias || app.name)} unlinked`);
   }
 };
