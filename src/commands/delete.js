c8e5c93ba92b6d1b0bd6783fae591f04bc48302f
David Legrand <david.legrand@clever-cloud.com>
2025-06-08 11:45:36 +0200

    feat(delete): improve display (texts and colors)

--- src/commands/delete.js
+++ src/commands/delete.js
@@ -1,6 +1,7 @@
 import * as AppConfig from '../models/app_configuration.js';
 import * as Application from '../models/application.js';
 import { Logger } from '../logger.js';
+import colors from 'colors/safe.js';
 
 export async function deleteApp (params) {
   const { alias, app: appIdOrName, yes: skipConfirmation } = params.options;
@@ -13,11 +14,12 @@ export async function deleteApp (params) {
   else {
     // delete app
     await Application.deleteApp(app, skipConfirmation);
-    Logger.println('The application has been deleted');
-    // unlink app
+    Logger.printSuccess(`Application ${colors.green(colors.bold(`${app.name}`))} successfully deleted!`);
+    Logger.println(`  ${colors.grey('•')} Application ID: ${colors.grey(app.id)}`);
+
     const wasUnlinked = await AppConfig.removeLinkedApplication({ appId, alias });
     if (wasUnlinked) {
-      Logger.println('The application has been unlinked');
+      Logger.println(`  ${colors.blue('→')} Local alias ${colors.blue(alias || app.name)} unlinked`);
     }
   }
 };
