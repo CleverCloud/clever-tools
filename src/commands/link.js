8aef5c493eaa20304436233255f5b112899b1da4
David Legrand <1110600+davlgd@users.noreply.github.com>
2025-08-20 21:11:53 +0200

    fix(link): throw an error if an application is already linked

--- src/commands/link.js
+++ src/commands/link.js
@@ -7,6 +7,13 @@ export async function link (params) {
   const [app] = params.args;
   const { org: orgaIdOrName, alias } = params.options;
 
+  let { apps } = await AppConfig.loadApplicationConf();
+
+  const existingApp = apps.find((a) => a.app_id === app.app_id);
+  if (existingApp) {
+    throw new Error(`Application is already linked to local alias ${colors.red(existingApp.alias)}`);
+  }
+
   if (app.app_id != null && orgaIdOrName != null) {
     Logger.warn('You\'ve specified a unique application ID, organisation option will be ignored');
     await Application.linkRepo(app, null, alias);
@@ -15,7 +22,7 @@ export async function link (params) {
     await Application.linkRepo(app, orgaIdOrName, alias);
   }
 
-  const { apps } = await AppConfig.loadApplicationConf();
+  ({ apps } = await AppConfig.loadApplicationConf());
   const attributedAlias = apps.find((a) => a.app_id === app.app_id)?.alias;
 
   Logger.printSuccess(`Application has been successfully linked to local alias ${colors.green(attributedAlias)}!`);
