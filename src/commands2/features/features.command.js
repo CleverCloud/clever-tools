ca706dcead064d4c02642b7b5753a524e47810be
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2025-12-14 02:30:33 +0100

    refactor: deduplicate command implementations when root and list are the same

--- src/commands2/features/features.command.js
+++ src/commands2/features/features.command.js
@@ -1,49 +1,7 @@
-import { EXPERIMENTAL_FEATURES } from '../../experimental-features.js';
-import { formatTable } from '../../format-table.js';
 import { defineCommand } from '../../lib/define-command.js';
-import { Logger } from '../../logger.js';
-import { getFeatures } from '../../models/configuration.js';
-import { humanJsonOutputFormatOption } from '../global.options.js';
+import { featuresListCommand } from './features.list.command.js';
 
 export const featuresCommand = defineCommand({
+  ...featuresListCommand,
   description: 'Manage Clever Tools experimental features',
-  since: '3.11.0',
-  options: {
-    format: humanJsonOutputFormatOption,
-  },
-  args: [],
-  async handler(options) {
-    const { format } = options;
-
-    const featuresConf = await getFeatures();
-    // Add status from configuration file and remove instructions
-    const features = Object.entries(EXPERIMENTAL_FEATURES).map(([id, feature]) => {
-      const enabled = featuresConf[id] === true;
-      return {
-        id,
-        status: feature.status,
-        description: feature.description,
-        enabled,
-      };
-    });
-
-    // For each feature, print the object with the id, status, description and enabled
-    switch (format) {
-      case 'json': {
-        Logger.printJson(features);
-        break;
-      }
-      case 'human':
-      default: {
-        const headers = ['ID', 'STATUS', 'DESCRIPTION', 'ENABLED'];
-
-        Logger.println(
-          formatTable([
-            headers,
-            ...features.map((feature) => [feature.id, feature.status, feature.description, feature.enabled]),
-          ]),
-        );
-      }
-    }
-  },
 });
