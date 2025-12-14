15d9a975027c058bfc66a0778dbc6b19512b4e66
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2025-12-14 02:18:33 +0100

    refactor: move single-usage options to their respective commands

--- src/commands2/env/env.options.js
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-import { defineOption } from '../../lib/define-option.js';
-
-export const sourceableEnvVarsListOption = defineOption({
-  name: 'add-export',
-  schema: z.boolean().default(false),
-  description: 'Display sourceable env variables setting',
-});
