9a097cadcc4464f1f89474c6fdf7984ee8946f56
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2025-12-17 17:16:43 +0100

    refactor: remove "old" CLI structure (clever.js and commands)

--- src/commands/makeDefault.js
+++
@@ -1,9 +0,0 @@
-import { styleText } from '../lib/style-text.js';
-import { Logger } from '../logger.js';
-import * as AppConfig from '../models/app_configuration.js';
-
-export async function makeDefault(_options, alias) {
-  await AppConfig.setDefault(alias);
-
-  Logger.printSuccess(`The application ${styleText('green', alias)} has been set as default`);
-}
