be1ab98d78d1caa2551fe8a9680a213afb493b0c
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2026-01-09 08:50:42 +0100

    chore(notify-email): make getEmailNotificationTargets function private
    Change function from exported to internal by removing export keyword.
    It helps future refactorings.

--- src/commands/notify-email.js
+++ src/commands/notify-email.js
@@ -48,7 +48,7 @@ export async function list(params) {
   }
 }
 
-export function getEmailNotificationTargets(notifTargets) {
+function getEmailNotificationTargets(notifTargets) {
   if (notifTargets == null) {
     return [];
   }
