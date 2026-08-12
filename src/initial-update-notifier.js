70a22084995fe72c73f54866389e826f88b9e812
Mehdi <26483210+mehdi653@users.noreply.github.com>
2026-08-12 22:06:47 +0000

    fix: update notifier message for package installs

--- src/initial-update-notifier.js
+++ src/initial-update-notifier.js
@@ -5,14 +5,13 @@ import { hasParam } from './lib/has-param.js';
 // These need to be set before Logger and other stuffs
 const updateNotifierExplicitFalse = hasParam('--no-update-notifier') || hasParam('--update-notifier', 'false');
 if (!updateNotifierExplicitFalse) {
+  const docsUrl = 'https://github.com/CleverCloud/clever-tools/tree/master/docs#how-to-use-clever-tools';
+  // update-notifier ignores getDetails/tagsUrl. Use the supported message template instead
+  // so brew/apt/binary users are not told to run npm i -g.
   updateNotifierModule({
     pkg,
-    tagsUrl: 'https://api.github.com/repos/CleverCloud/clever-tools/tags',
   }).notify({
     isGlobal: true,
-    getDetails() {
-      const docsUrl = 'https://github.com/CleverCloud/clever-tools/tree/master/docs#how-to-use-clever-tools';
-      return `\nPlease follow this link to update your clever-tools:\n${docsUrl}`;
-    },
+    message: `Update available {currentVersion} -> {latestVersion}\nPlease follow this link to update your clever-tools:\n${docsUrl}`,
   });
 }
